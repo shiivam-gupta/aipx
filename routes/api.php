17a2a9b44ccda1555acdc445c590a140fc44197b
PraveenKishore <praveenkishore.ec@gmail.com>
2020-04-29 17:18:52 +0530

    changes in web

--- routes/api.php
+++ routes/api.php
@@ -21,9 +21,7 @@ use Illuminate\Support\Facades\Route;
 // });
  
 // Route::middleware('auth:api')->group(function () {
-//     Route::get('user', 'PassportController@details');
- 
-//     Route::resource('products', 'ProductController');
+	
 // });
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
